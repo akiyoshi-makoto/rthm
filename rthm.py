de0cec30533fb34b22952830c55fd803cc3d3f06
Akiyoshi Makoto <makoto.akiyosi@gmail.com>
2021-01-16 20:25:41 +0900

    顔検出処理のカスタマイズ

--- rthm.py
+++ rthm.py
@@ -11,25 +11,23 @@ import cv2
 import numpy as np
 import PIL.Image, PIL.ImageTk
 
-############################################################
+##############################################################################
 # 定数
-############################################################
+##############################################################################
 TRIG = 27
 ECHO = 22
-CYCLE_TIME = 100            # 処理周期[msec]
+CYCLE_TIME = 50             # 処理周期[msec]
 DISTANCE_DEFAULT = 50       # 対象物までの距離(デフォルト値)
 I2C_ADR = 0x68              # I2C アドレス
-FACE_DETECTIION_PAUSE = 10  # 顔検出時の一時停止周期
+FACE_DETECTIION_PAUSE = 40  # 顔検出時の一時停止周期
 
-############################################################
-# オプション設定
-############################################################
+##############################################################################
+# オプション設定 True:有効 False:無効
+##############################################################################
 # 超音波センサ(HC-SR04)
-ENABLE_ULTRA_SONIC_SENSOR = False   # True:有効 False:無効
+ENABLE_ULTRA_SONIC_SENSOR = False
 # サーマルカメラ(AMG8833)
-ENABLE_THERMAL_CAMERA = True       # True:有効 False:無効     
-# 顔検出制御
-ENABLE_FACE_DETECTIION = True       # True:有効 False:無効
+ENABLE_THERMAL_CAMERA = True     
 
 class Application(ttk.Frame):
     def __init__(self, master=None):
@@ -38,17 +36,21 @@ class Application(ttk.Frame):
         self.pack()
         # ウィンドウをスクリーンの中央に配置
         self.setting_window(master)
+
+        self.cycle_proc_exec = True     # 周期処理実行許可フラグ(True:許可 False:禁止)
+        self.pause_timer = 0            # 顔検出時の一時停止タイマ 
+        self.thermal_exec = False       # サーマルカメラ実行要求フラグ(True:要求有 False:要求無)
+
         # ウィジットを生成
         self.create_widgets()
         # デバイスの初期化
         self.init_device()
         # 周期処理
-        self.cycle_proc_exec = True
         self.cycle_proc()
 
-    ############################################################
+    ##########################################################################
     # ウィンドウをスクリーンの中央に配置
-    ############################################################
+    ##########################################################################
     def setting_window(self, master):
         w = 500                             # ウィンドウの横幅
         h = 700                             # ウィンドウの高さ
@@ -60,9 +62,9 @@ class Application(ttk.Frame):
         master.minsize(w,h)
         master.title('体温測定システム')
     
-    ############################################################
+    ##########################################################################
     # ウィジットを生成
-    ############################################################
+    ##########################################################################
     def create_widgets(self):
         # フレーム(カメラ)
         frame_camera = ttk.Frame(self)
@@ -94,53 +96,24 @@ class Application(ttk.Frame):
             self.label_distance = ttk.Label(frame_data, text='対象物までの距離：')
             self.label_distance.grid(row=4, sticky='NW')
 
-    ############################################################
+    ##########################################################################
     # デバイスの初期化
-    ############################################################
+    ##########################################################################
     def init_device(self):   
         # 超音波センサ(HC-SR04)
         if ENABLE_ULTRA_SONIC_SENSOR:
             self.init_ultra_sonic_sensor()
         else:
-            self.distance = DISTANCE_DEFAULT    # 対象物までの距離
+            self.distance = DISTANCE_DEFAULT
         # サーマルカメラ(AMG8833)
         if ENABLE_THERMAL_CAMERA:
             self.init_thermal_camera()
         # ビデオカメラ
         self.init_video_camera()
 
-    ############################################################
-    # 周期処理
-    ############################################################
-    def cycle_proc(self):
-        # 周期処理実行許可
-        if self.cycle_proc_exec:
-            # 超音波センサ(HC-SR04)
-            if ENABLE_ULTRA_SONIC_SENSOR:
-                self.ctrl_ultra_sonic_sensor()
-            # サーマルカメラ(AMG8833)
-            if ENABLE_THERMAL_CAMERA:
-                self.ctrl_thermal_camera()
-            # ビデオカメラ
-            self.ctrl_video_camera()
-            # 周期処理
-            self.after(CYCLE_TIME, self.cycle_proc)
-
-    ############################################################
-    # 閉じるボタンが押下された場合の処理
-    ############################################################
-    def on_close_button(self):
-        # 周期処理実行禁止
-        self.cycle_proc_exec = False
-        # 終了処理
-        GPIO.cleanup()
-        self.video_camera.release()
-        # メインウインドウを閉じる
-        self.master.destroy()
-
-    ############################################################
+    ##########################################################################
     # 超音波センサ(HC-SR04) 初期化
-    ############################################################
+    ##########################################################################
     def init_ultra_sonic_sensor(self):
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
@@ -148,9 +121,9 @@ class Application(ttk.Frame):
         GPIO.setup(ECHO, GPIO.IN)
         GPIO.output(TRIG, GPIO.LOW)
 
-    ############################################################
+    ##########################################################################
     # 超音波センサ(HC-SR04) 制御
-    ############################################################
+    ##########################################################################
     def ctrl_ultra_sonic_sensor(self):
         # Trig端子を10us以上High
         GPIO.output(TRIG, GPIO.HIGH)
@@ -169,9 +142,9 @@ class Application(ttk.Frame):
 
         self.label_distance.config(text='対象物までの距離：' + str(round(self.distance)))
 
-    ############################################################
+    ##########################################################################
     # サーマルカメラ(AMG8833) 初期化
-    ############################################################
+    ##########################################################################
     def init_thermal_camera(self):   
         # I2Cバスの初期化
         i2c_bus = busio.I2C(board.SCL, board.SDA)
@@ -199,9 +172,9 @@ class Application(ttk.Frame):
             #i2c.write_byte_data(I2C_ADR, 0x07, 0x00)
             #i2c.write_byte_data(I2C_ADR, 0x1F, 0x00)
 
-    ############################################################
+    ##########################################################################
     # サーマルカメラ(AMG8833) 制御
-    ############################################################
+    ##########################################################################
     def ctrl_thermal_camera(self):
         pixels_array = np.array(self.sensor.pixels)
         pixels_ave = np.average(pixels_array)
@@ -225,58 +198,92 @@ class Application(ttk.Frame):
         
         print(pixels_array)
 
-    ############################################################
+    ##########################################################################
     # ビデオカメラ　初期化
-    ############################################################
+    ##########################################################################
     def init_video_camera(self):   
         self.video_camera = cv2.VideoCapture(0)
         self.video_camera.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
         self.video_camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
         # 顔検出のための学習元データを読み込む
-        if ENABLE_FACE_DETECTIION:
-            self.pause_timer = 0        # 顔検出時の一時停止タイマ 
-            self.face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+        self.face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
 
-    ############################################################
+    ##########################################################################
     # ビデオカメラ 制御
-    ############################################################
+    ##########################################################################
     def ctrl_video_camera(self):
         if self.video_camera.isOpened():
-            # ビデオカメラの停止画を取得
-            _, frame = self.video_camera.read()
-            # 左右反転
-            frame_mirror = cv2.flip(frame, 1)
-            # OpenCV(BGR) -> Pillow(RGB)変換
-            frame_color = cv2.cvtColor(frame_mirror, cv2.COLOR_BGR2RGB)
-        
-            # 顔検出制御
-            if ENABLE_FACE_DETECTIION:
-                if self.pause_timer > 0:
-                    self.pause_timer -= 1
-                else:
-                    # 顔検出の処理効率化のために、写真の情報量を落とす（モノクロにする）
-                    frame_gray = cv2.cvtColor(frame_color, cv2.COLOR_BGR2GRAY)
-                    # 顔検出を行う(detectMultiScaleの戻り値は(x座標, y座標, 横幅, 縦幅)のリスト)
-                    facerect = self.face_cascade.detectMultiScale(frame_gray, scaleFactor=1.2, minNeighbors=2, minSize=(350, 350))
-                    # 顔が検出された場合
-                    if len(facerect) > 0:
-                        print(facerect)
-                        self.pause_timer = FACE_DETECTIION_PAUSE
-                        # 検出した場所すべてに緑色で枠を描画する
-                        for rect in facerect:
-                            cv2.rectangle(frame_color, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]), (0, 255, 0), thickness=3)
-                    
-                    # OpenCV frame -> Pillow Photo
-                    self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame_color))
-                    # Pillow Photo -> Canvas
-                    self.canvas.create_image(0, 0, image = self.photo, anchor = 'nw')
 
+            # ビデオカメラの停止画を取得
+            ret, frame = self.video_camera.read()
+
+            if self.pause_timer > 0:
+                self.pause_timer -= 1
             else:
+                # 左右反転
+                frame_mirror = cv2.flip(frame, 1)
+                # OpenCV(BGR) -> Pillow(RGB)変換
+                frame_color = cv2.cvtColor(frame_mirror, cv2.COLOR_BGR2RGB)
+                # 顔検出の処理効率化のために、写真の情報量を落とす（モノクロにする）
+                frame_gray = cv2.cvtColor(frame_color, cv2.COLOR_BGR2GRAY)
+                # 顔検出を行う(detectMultiScaleの戻り値は(x座標, y座標, 横幅, 縦幅)のリスト)
+                facerect = self.face_cascade.detectMultiScale(frame_gray,
+                                                              scaleFactor=1.2,
+                                                              minNeighbors=2,
+                                                              minSize=(320, 320))
+                # 顔が検出された場合
+                if len(facerect) > 0:
+                    # 一時停止してその間にサーマルカメラ制御を実行する
+                    self.pause_timer = FACE_DETECTIION_PAUSE
+                    # サーマルカメラ実行要求
+                    self.thermal_exec = True
+                    # 検出した場所すべてに緑色で枠を描画する
+                    for rect in facerect:
+                        cv2.rectangle(frame_color,
+                                      tuple(rect[0:2]),
+                                      tuple(rect[0:2]+rect[2:4]),
+                                      (0, 255, 0),
+                                      thickness=3)
+
+                    print(facerect)
+                else:
+                    self.pause_timer = 0
+
                 # OpenCV frame -> Pillow Photo
                 self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame_color))
                 # Pillow Photo -> Canvas
                 self.canvas.create_image(0, 0, image = self.photo, anchor = 'nw')
+
+    ##########################################################################
+    # 周期処理
+    ##########################################################################
+    def cycle_proc(self):
+        # 周期処理実行許可
+        if self.cycle_proc_exec:
+            # 超音波センサ(HC-SR04)
+            if ENABLE_ULTRA_SONIC_SENSOR:
+                self.ctrl_ultra_sonic_sensor()
+            # サーマルカメラ(AMG8833)
+            if ENABLE_THERMAL_CAMERA:
+                self.ctrl_thermal_camera()
+            # ビデオカメラ
+            self.ctrl_video_camera()
+            # 周期処理
+            self.after(CYCLE_TIME, self.cycle_proc)
+
+    ##########################################################################
+    # 閉じるボタンが押下された場合の処理
+    ##########################################################################
+    def on_close_button(self):
+        # 周期処理実行禁止
+        self.cycle_proc_exec = False
+        # 終了処理
+        if ENABLE_ULTRA_SONIC_SENSOR:
+            GPIO.cleanup()
+        self.video_camera.release()
+        # メインウインドウを閉じる
+        self.master.destroy()
 
 if __name__ == '__main__':
     root = Tk()
