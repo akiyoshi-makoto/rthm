d7c801505369b5c9e32ee7156aac95c5a52f4bfe
Akiyoshi Makoto <makoto.akiyosi@gmail.com>
2021-02-07 14:52:11 +0900

    補正なしを採用

--- trial/thermo_graphy.py
+++ trial/thermo_graphy.py
@@ -24,15 +24,15 @@ thermistor_temp = round(sensor.temperature, 1)
 try:
     while True:
         # 8x8センサアレイ内の最大温度を取得
-        pixels_array = np.array(sensor.pixels)
+        # pixels_array = np.array(sensor.pixels)
         # サーミスタ温度補正
-        offset_temp = round((0.8424 * thermistor_temp - 3.2523), 2)
+        # offset_temp = round((0.8424 * thermistor_temp - 3.2523), 2)
         # 体温
-        body_temp_array = pixels_array + offset_temp
-        body_temp_max = round(np.amax(body_temp_array), 1)
+        # body_temp_array = pixels_array + offset_temp
+        # body_temp_max = round(np.amax(body_temp_array), 1)
  
         plt.subplot(1,2,1)
-        plt.imshow(body_temp_array, cmap="inferno", interpolation="bicubic",vmin=30,vmax=40)
+        plt.imshow(sensor.pixels, cmap="inferno", interpolation="bicubic",vmin=20,vmax=30)
         plt.colorbar()
         plt.show
         plt.draw()
